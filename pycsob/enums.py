afd1b6e5a7d66035a3eee25c1cba2bc33213eb71
Ondřej Kulatý <ondrej.kulaty@skippay.cz>
2025-06-11 10:45:43 +0200

    Fix URLs

--- pycsob/enums.py
+++ pycsob/enums.py
@@ -2,13 +2,13 @@ from enum import Enum
 
 
 class EndpointUrl(Enum):
-    CUSTOMER_INFO = 'customer/info'
-    ECHO = 'echo'
-    ONE_CLICK_INIT = 'oneclick/init'
-    ONE_CLICK_PROCESS = 'oneclick/process'
-    PAYMENT_CLOSE = 'payment/close'
-    PAYMENT_INIT = 'payment/init'
-    PAYMENT_PROCESS = 'payment/process'
-    PAYMENT_REFUND = 'payment/refund'
-    PAYMENT_REVERSE = 'payment/reverse'
-    PAYMENT_STATUS = 'payment/status'
+    CUSTOMER_INFO = 'customer/info/'
+    ECHO = 'echo/'
+    ONE_CLICK_INIT = 'oneclick/init/'
+    ONE_CLICK_PROCESS = 'oneclick/process/'
+    PAYMENT_CLOSE = 'payment/close/'
+    PAYMENT_INIT = 'payment/init/'
+    PAYMENT_PROCESS = 'payment/process/'
+    PAYMENT_REFUND = 'payment/refund/'
+    PAYMENT_REVERSE = 'payment/reverse/'
+    PAYMENT_STATUS = 'payment/status/'
